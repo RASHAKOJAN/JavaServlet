f8199c563de3c295ade32bb0e468e92bac8fbb44
RASHAKOJAN <rashakoujan@outlook.com>
2023-11-13 04:37:43 +0300

    Dependency Injection : by Setter Method.

--- src/main/java/Containers/Employee.java
+++ src/main/java/Containers/Employee.java
@@ -1,10 +1,81 @@
 package Containers;
 
+import java.util.List;
+
 public class Employee {
 
+    // Dependency Injection (DI) is a design pattern used to implement IoC. allows the creation of dependent objects
+    //through different ways.
+    private Long id;
     private String name;
+    private Double salary;
+    private Department department;
 
-    public void printName(String name){
-        System.out.println(name);
+    private List<String> address;
+
+    private List<Book> books;
+
+    // There are two ways to perform Dependency Injection in Spring framework:
+    // The first one is by Constructor and  the last one is by Setter Methods
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<String> address) {
+        this.address = address;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public void printMethod(){
+        System.out.println("Employee Name:" + this.name + "ID:" + this.id);
+        System.out.println("Department Name:" + department.getName() + "ID:" + department.getId());
+
+        for(String item: address){
+            System.out.println("Address is :" + item);
+        }
+
+        for(Book item: books){
+            System.out.println("Book is :" + item.getName());
+        }
     }
 }
--- src/main/java/Containers/IocContainer.java
+++ src/main/java/Containers/IocContainer.java
@@ -5,16 +5,22 @@ import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import Containers.Employee;
 
 public class IocContainer {
     // There are two types of IoC (Inversion of Control) containers: make injection and init of beans when we call it
     //BeanFactory  --simple container using in simple projects
 
-    BeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-    Employee employee = (Employee) factory.getBean("emp");
+    public static void main(String[] args) {
+        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        Employee employee = (Employee) factory.getBean("emp");
+        employee.printMethod();
 
-    //ApplicationContext  -- more functionality built on top of the BeanFactory interface
-    // method of implementation FileSystemXmlApplicationContext(locate from anywhere)- ClassPathXmlApplicationContext - WebXmlApplicationContext
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-    Employee employee2 = (Employee) applicationContext.getBean("emp");
+        //ApplicationContext  -- more functionality built on top of the BeanFactory interface
+        // method of implementation FileSystemXmlApplicationContext(locate from anywhere)- ClassPathXmlApplicationContext - WebXmlApplicationContext
+        /*ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Employee employee2 = (Employee) applicationContext.getBean("emp");
+
+        employee2.printMethod();*/
+    }
 }
